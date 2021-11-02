c80ae4f027286140fa60dd47b65b4a9205ca746f
javierpb275 <javierpb275@gmail.com>
2021-11-02 16:35:42 +0100

    error handling 3

--- error-handling.py
+++ error-handling.py
@@ -1,17 +1,19 @@
 # error handling
 
-""" while True:
-    try:
-        age = int(input("what's your age?"))
-        10/age
-    except ValueError:
-        print('please enter a number')
-    except ZeroDivisionError:
-        print('please enter a number bigger than 0')
-    else:
-        print('thank you')
-        break
- """
+def error_handling1():
+    while True:
+        try:
+            age = int(input("what's your age?"))
+            10/age
+        except ValueError:
+            print('please enter a number')
+        except ZeroDivisionError:
+            print('please enter a number bigger than 0')
+        else:
+            print('thank you')
+            break
+
+# error_handling1()
 
 # error handling 2:
 
@@ -23,4 +25,27 @@ def sum(num1, num2):
         print(err)
 
 
-print(sum('a', 2))
+#print(sum('a', 2))
+
+
+# error handling 3:
+
+def error_handling3():
+    while True:
+        try:
+            age = int(input("what's your age?"))
+            10/age
+        except ValueError:
+            print('please enter a number')
+            continue
+        except ZeroDivisionError:
+            print('please enter a number bigger than 0')
+            break
+        else:
+            print('thank you')
+        finally:
+            print('ok, i am done')
+        print('can you hear me?')
+
+
+error_handling3()
